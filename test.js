a655fe13bbf6d8daa288c4526f865548b3aa28c1
Ivan Herman <ivan@ivan-herman.net>
2018-06-08 16:51:36 +0200

    Two nasty bugs...
    - the usage of hashTuple was wrong in the HashBag class (is now
    bypassed)
    - wrong value was returned from the hashBnodes function.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 const _             = require("underscore");
 const fs            = require('fs');
 const readline      = require('readline');
-const debug         = true;
+const program 		= require('commander');
 
 const { HashValue, hashTerm, hashTuple, hashBag } = require('./lib/Hash');
 const { HashTable }                               = require('./lib/HashTable');
@@ -12,7 +12,7 @@ const { isoCanonicalize }                         = require('./lib/iso_canonical
 
 const test_file = "test_files/test000.nq";
 
-function perform_test(numb) {
+function perform_test(numb, run_with_debug) {
     let base            = (numb.length === 1) ? `test00${numb}` : `test0${numb}`;
     let test_file       = `test_files/${base}.nq`;
     let test_triples    =   "";
@@ -20,29 +20,34 @@ function perform_test(numb) {
     console.log(`>>> Test triples from ${test_file}:\n${test_triples}`);
 
     let graph       = new Dataset().parse(test_triples);
-    let can_dataset = isoCanonicalize(graph)
+    let can_dataset = isoCanonicalize(graph, run_with_debug)
 
     console.log(`>>> Calculated triples:\n${can_dataset.toString()}`);
-
 }
 
 // ---------------------- See if one or more numbers have been provided on the command line:
 
-if( process.argv.length === 2 ) {
-    perform_test("0");
+let usage = '[-d|-c] [number]'
+program
+   .usage(usage)
+   .option('-d --debug','run in debug mode')
+   .option('-c --cycle','run in a cycle asking for test numbers')
+   .parse(process.argv);
+
+let debug = program.debug ? true : false;
+let cycle = program.cycle ? true : false;
+
+if(program.args.length === 0) {
+    perform_test("0", debug);
 } else {
-    process.argv.forEach( (val, index) => {
-        if( index > 1 ) {
-            perform_test(val);
-            if( index !== process.argv.length - 1 ) console.log("====")
-        }
+    program.args.forEach( (val, index) => {
+        perform_test(val, debug);
+        if( index !== program.args.length - 1 ) console.log("\n====\n")
     })
 }
 
-
-
 // ---------------------- Cycle for additional tests
-if( !debug ) {
+if( cycle ) {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
@@ -54,7 +59,7 @@ if( !debug ) {
             rl.close();
             process.exit(0)
         } else {
-            perform_test(answer);
+            perform_test(answer, debug);
             console.log("==== Test number? ('q' to quit) ")
         }
     });
